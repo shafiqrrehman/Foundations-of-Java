0acb9230ca4a5be7777cadc1dd04e184c7e19e32
shafiqrrehman <shafiq_1309e@aptechgdn.com>
2018-05-10 18:43:31 +0500

    Preincrementing and postincrementing.

---
+++ Increament/Increament.java
@@ -0,0 +1,17 @@
+public class Increament {
+
+	public static void main(String[] args) {
+		
+		int c = 5;
+		System.out.println(c);
+		System.out.println(c++);
+		System.out.println(c);
+
+		System.out.println();
+
+		c = 5;
+		System.out.println(c);
+		System.out.println(++c);
+		System.out.println(c);
+	}
+}
