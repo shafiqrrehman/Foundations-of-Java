56b3f6b436a5d084ffd5d84c8e3f2a962e742b7c
shafiqrrehman <shafiq_1309e@aptechgdn.com>
2018-07-03 11:07:58 +0500

    Create GradeBook object, input grades and display grade report.

--- GradebookApp/GradebookTest.java
+++ GradebookApp/GradebookTest.java
@@ -8,20 +8,9 @@ public class GradebookTest {
 
         Gradebook myGradebook = new Gradebook("CS101 Introduction to Java Programming");
 
-
-        // System.out.printf("gradeBook1 course name is: %s\n\n", myGradebook1.getCourseName());
-        // System.out.printf("gradeBook1 course instructor is: %s\n\n", myGradebook1.getCourseInstructor());
-        // System.out.printf("Initial course name is: %s\n\n", myGradebook.getCourseName);
-
-        // System.out.println("Enter your Gradebook name ?");
-        // String gradeBook = input.nextLine();
-        // System.out.println();
-
-        // myGradebook.setCourseName(gradeBook);
-
         myGradebook.displayMessage();
         myGradebook.inputGrades();
         myGradebook.displayGradeReport();
-        // myGradebook.determineClassAverage();
+
     }
 }
