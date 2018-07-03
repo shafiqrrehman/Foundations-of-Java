0ef74abf57e7f97d59111c8ed52645d55f3c18f5
shafiqrrehman <shafiq_1309e@aptechgdn.com>
2018-07-03 16:31:31 +0500

    continue statement terminating an iteration of a for statement.

---
+++ Mystery/ContinueTest.java
@@ -0,0 +1,17 @@
+public class ContinueTest {
+
+	public static void main(String[] args) {
+		
+		int count;
+
+		for (count = 1; count <= 10; count++) {
+			
+			if(count == 5)
+				continue;
+
+			System.out.printf("%d ", count);
+		}
+
+		System.out.printf("\nUsed continue to skip printing 5");
+	}
+}
