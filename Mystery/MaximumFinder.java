c00aec30a120417af73181e25dd34f3d0d90cd8e
shafiqrrehman <shafiq_1309e@aptechgdn.com>
2018-07-10 19:05:32 +0500

    Declared method maximum with three double parameterss to find max value among them.

---
+++ Mystery/MaximumFinder.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class MaximumFinder {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+
+		System.out.print("Enter three floating-point value separated by space: ");
+
+		double num1 = input.nextDouble();
+		double num2 = input.nextDouble();
+		double num3 = input.nextDouble();
+
+		double result = maximum(num1, num2, num3);
+
+		System.out.println("Maximum value: " + result);
+
+	}
+
+	public static double maximum(double x, double y, double z) {
+
+		double maximumValue = x;
+
+		if(y > maximumValue)
+			maximumValue = y;
+
+		if(z > maximumValue)
+			maximumValue = z;
+
+		return maximumValue;
+	}
+}
