20468e9e20a4db55b8527d9e672c1bb9fd4f31bd
shafiqrrehman <shafiq_1309e@aptechgdn.com>
2018-07-21 11:55:40 +0500

    Poll analysis program using Arrays to Analyze survey results.

--- Arrays/StudentPoll.java
+++ Arrays/StudentPoll.java
@@ -1,3 +1,15 @@
+/*  
+	Problem:
+	
+	Twenty students were asked to rate on a scale of 1 to 5 the quality of the food in the
+	student cafeteria, with 1 being “awful” and 5 being “excellent.” Place the 20 responses
+	in an integer array and determine the frequency of each rating.
+*/
+
+	/*
+		PROBLEM SOLVED BY: SHAFIQ UR REHMAN BALOCH
+	*/
+
 public class StudentPoll {
 
 	public static void main(String[] args) {
