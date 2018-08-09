898aed553d754852173b38d8d4a116c47ee062ea
shafiqrrehman <shafiq_1309e@aptechgdn.com>
2018-08-09 16:58:02 +0500

    Using variable length argument lists.

--- Arrays/VariableLengthArgument.java
+++ Arrays/VariableLengthArgument.java
@@ -7,7 +7,7 @@ public class VariableLengthArgument {
 		double d3 = 30.1;
 		double d4 = 40.5;
 
-		System.out.printf( "\nd1 = %.2f\nd2 = %.2f\nd3 = %.2f\nd4 = %.2f\n", 
+		System.out.printf( "\nd1 = %.2f\nd2 = %.2f\nd3 = %.2f\nd4 = %.2f\n\n", 
 			d1, d2, d3, d4 );
 
 		System.out.printf( "Average of d1 and d2 is %.2f\n", average( d1, d2 ) );
@@ -17,14 +17,15 @@ public class VariableLengthArgument {
 		System.out.printf( "Average of d1, d2, d3 and d4 is %.2f\n", average( d1, d2, d3, d4 ) );
 	}
 
+	// calculate average
 	public static double average ( double... numbers ) {
 
-		double total = 0.0;
+		double total = 0.0; // intialize total
 
 		// calculate total by enhanced for loop
 		for ( double d : numbers )
 			total += d;
 
 		return total / numbers.length; // return average
-	}
-}
+	} // end average method
+} // end VariableLengthargument Lists class
