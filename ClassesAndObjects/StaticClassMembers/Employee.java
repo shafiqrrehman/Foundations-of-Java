270063cf0b20602a259b12774d22177e22504a52
shafiqrrehman <shafiq_1309e@aptechgdn.com>
2018-08-28 11:48:22 +0500

    static variable used to maintain a count of the number of Employee objects in memory.

---
+++ ClassesAndObjects/StaticClassMembers/Employee.java
@@ -0,0 +1,32 @@
+public class Employee {
+
+	private String firstName;
+	private String lastName;
+	private static int count = 0;
+
+	public Employee( String first, String last ) {
+
+		firstName = first;
+		lastName = last;
+
+		++count;
+
+		System.out.printf( "Employee constructor: %s %s; count = %d\n", 
+			firstName, lastName, count );
+	}
+
+	public String getFirstName() {
+
+		return firstName;
+	}
+
+	public String getLastName() {
+
+		return lastName;
+	}
+
+	public static int getCount() {
+
+		return count;
+	}
+}
