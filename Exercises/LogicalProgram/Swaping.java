ef111ba34910e2e2b8e1507177c19ad185b858cf
shafiqrrehman <shafiq_1309e@aptechgdn.com>
2018-11-20 23:10:04 +0500

    Logical programs about swaping.

---
+++ Exercises/LogicalProgram/Swaping.java
@@ -0,0 +1,14 @@
+public class Swaping {
+
+	public static void main(String[] args) {
+		
+		int a = 10, b = 20;
+		int t;
+
+		t = a;
+		a = b;
+		b = t;
+
+		System.out.println("A: " + a + "\n" + "B: " + b);
+	}
+}
