8d8ea6ebfd4fb5dd08d4c26ff1888a49e9c7f4ad
shafiqrrehman <shafiq_1309e@aptechgdn.com>
2018-11-21 20:22:29 +0500

    Factorial logic program.

--- Exercises/LogicalProgram/Factorial.java
+++ Exercises/LogicalProgram/Factorial.java
@@ -1,13 +1,18 @@
+import java.util.Scanner;
+
 public class Factorial {
 
 	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
 		
-		int num = 5;
+		System.out.print("Enter a number for it's Factorial: ");
+		int num = sc.nextInt();
 		int factorial = 1;
 
 		for (int i = 1; i <= num; i ++) {
 			factorial = factorial * i;
 		}
-		System.out.print(factorial); // 5 x 4 x 3 x 2 x 1 = 120 
+		System.out.print("Factorial of "+ num + " is " + factorial); // 5 x 4 x 3 x 2 x 1 = 120 
 	}
 }
