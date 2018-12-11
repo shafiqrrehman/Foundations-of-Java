c082de68ce1072dd8b3f796e388aca9786f1ec58
shafiqrrehman <shafiq_1309e@aptechgdn.com>
2018-12-11 12:05:14 +0500

    small while logic.

---
+++ while/Excercise.java
@@ -0,0 +1,15 @@
+class Excercise {
+
+	public static void main(String[] args) {
+		
+		int x = 1;
+
+		while( x < 10 ) {
+			if (x < 3) {
+				System.out.println("Big X");
+			}
+
+			x++;
+		}
+	}
+}
